e85edf4d253960ffba74eb0afd5b11c20fefc119
LobsterCat <1002127@palisadessd.net>
2018-09-25 12:17:42 -0400

    Update ExpressionsLab.java

--- ExpressionsLab.java
+++ ExpressionsLab.java
@@ -20,7 +20,7 @@ public class ExpressionsLab
   {
     // initializing all of the integers that will be used to test my answers
 	// in the 2.4 Lab
-    int iDiameter, iResult, iBase, iWeight, iNumOne, iNumTwo;
+    int iDiameter, iResult, iBase, iWeight, iNumOne, iNumTwo, result;
 
     // initializing all of the doubles required to test my answers
     // from the 2.4 Lab 
@@ -34,5 +34,26 @@ public class ExpressionsLab
     
     // checking my answers for number 4 in the 2.4 lab
     System.out.println();
+    System.out.println("Question 4 Answers");
+    System.out.println(15 + 7 * 3);
+    System.out.println((15 + 7) * 3);
+    System.out.println(3 * 6 + 10 /5 +5);
+    System.out.println(27 % 5 + 7 % 3);
+    System.out.println(100 /2 /2 /2);
+    System.out.println(100 / (2 / 2) /2);
+    
+    // checking answers for questions 5 and 6
+    // if a line of code is commented out, then it failed to compile
+    result = ( 5 + 2 );
+    //result = ( 5 + 2 * (15 - 3);
+    //result = ( 5 + 2 (;
+    //result = ( 5 + 2 ( 4 ) );
+    System.out.println();
+    System.out.println("Answers for Number 6");
+    System.out.println(iResult = 27);
+    System.out.println(iResult = iResult + 3);
+    System.out.println(iResult = iResult / 7);
+    System.out.println(iResult = iResult * 2);
+    
   }
 }
