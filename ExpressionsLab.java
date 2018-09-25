e886be395046a5694f91fbd7e3bddde35d694c2d
LobsterCat <1002127@palisadessd.net>
2018-09-25 12:06:07 -0400

    Create ExpressionsLab.java

---
+++ ExpressionsLab.java
@@ -0,0 +1,38 @@
+/**
+ * ExpressionsLab.java
+ *
+ * Code Description: This is the place where you will give a reasonable
+ * amount of information about your code that helps the reader understand
+ * what the code should do.
+ *    
+ * @author Jacob Robbins
+ * @version 9/25/18
+ * @contact 1002127@palisadessd.net
+ */
+
+public class ExpressionsLab
+{
+  /**
+   * main method - 
+   * @param args
+   */
+  public static void main (String[] args)
+  {
+    // initializing all of the integers that will be used to test my answers
+	// in the 2.4 Lab
+    int iDiameter, iResult, iBase, iWeight, iNumOne, iNumTwo;
+
+    // initializing all of the doubles required to test my answers
+    // from the 2.4 Lab 
+    double dResult, dValOne, dValTwo; 
+    
+    // testing questions 1-3 and comparing my answers
+    iDiameter = 5;
+    System.out.println(19 % 5);
+    System.out.println(13 / 4);
+    System.out.println(iDiameter = iDiameter * 4);
+    
+    // checking my answers for number 4 in the 2.4 lab
+    System.out.println();
+  }
+}
