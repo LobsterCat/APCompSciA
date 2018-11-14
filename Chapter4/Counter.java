774a4c5eddea6065f916496e46fc8706932f9442
LobsterCat <jacobro47@gmail.com>
2018-11-14 10:36:17 -0500

    Create Counter.java

---
+++ Chapter4/Counter.java
@@ -0,0 +1,81 @@
+/**
+ * Counter.java
+ *
+ * Code Description: Creates a counter with various ways of incrementing
+ * 
+ * @author Jacob Robbins
+ * @version 11/14/18
+ * @contact 1002127@palisadessd.net
+ */
+
+public class Counter
+{
+  // declare variable to represent the counter
+  private int iCount;
+
+  /**
+   * Constructor - Sets the counter to zero
+   * 
+   * @param
+   */
+  public Counter()
+  {
+    iCount = 0;
+  }
+
+  /**
+   * click - increments the counter by the parameter
+   * 
+   * @param iIncrement
+   *          gets added to iCount
+   */
+  public int click(int iIncrement)
+  {
+    iCount += iIncrement;
+    return iCount;
+  }
+
+  /**
+   * getCount - returns the counter's value
+   * 
+   * @param
+   */
+  public int getCount()
+  {
+    return iCount;
+  }
+
+  /**
+   * resetCount - Sets the counter to zero
+   * 
+   * @param
+   */
+  public void resetCount()
+  {
+    iCount = 0;
+  }
+
+  /**
+   * toString - Converts the integer to a String
+   * 
+   * @param
+   */
+  public String toString()
+  {
+    String sCount = Integer.toString(iCount);
+    return sCount;
+
+  }
+  
+  /**
+   * increment - Increases the counter by one
+   * 
+   * @param
+   */
+  public int increment()
+  {
+    iCount++;
+    return iCount;
+  }
+
+}
