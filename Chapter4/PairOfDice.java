51ed9ae581a915539608a3e1e456bca568b6dbbf
LobsterCat <jacobro47@gmail.com>
2018-11-19 10:14:59 -0500

    Create PairOfDice.java

---
+++ Chapter4/PairOfDice.java
@@ -0,0 +1,23 @@
+
+/**
+ * PairOfDice.java
+ *
+ * Code Description: 
+ * 
+ * @author Jacob Robbins
+ * @version 11/19/18
+ * @contact 1002127@palisadessd.net
+ */
+
+public class PairOfDice
+  {
+  
+  private DieV2 dieOne, dieTwo;
+    
+  public PairOfDice()
+  {
+    dieOne = new DieV2();
+    dieTwo = new DieV2();
+  }
+  
+}
