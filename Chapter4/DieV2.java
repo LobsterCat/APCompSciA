0d74b094b0066c5acb1e0cfa5ab216b0e432d354
LobsterCat <jacobro47@gmail.com>
2018-11-19 10:15:31 -0500

    Create DieV2.java

---
+++ Chapter4/DieV2.java
@@ -0,0 +1,71 @@
+/**
+ * Die.java
+ *
+ * Code Description: Class definitions for a playing Die
+ * 
+ * @author Mr. Einolf
+ * @version 11/08/18
+ * @contact teinolf@palisadessd.org
+ */
+
+public class DieV2
+{
+  // set maximum face value
+  private final int iMAX = 6;
+
+  // declare current value showing on the die
+  private int iFaceValue; 
+
+  /**
+   * Constructor - Sets the initial face value
+   * 
+   * @param 
+   */
+  public DieV2()
+  {
+    iFaceValue = 1;
+  }
+
+  /**
+   * roll - Rolls the die and returns the result
+   * 
+   * @param 
+   */
+  public int roll()
+  {
+    iFaceValue = (int) (Math.random() * iMAX) + 1;
+    return iFaceValue;
+  }
+
+  /**
+   * setFaceValue - Face value mutator
+   * 
+   * @param 
+   */
+  public void setFaceValue(int iValue)
+  {
+    iFaceValue = iValue;
+  }
+
+  /**
+   * getFaceValue - Face value accessor
+   * 
+   * @param 
+   */
+  public int getFaceValue()
+  {
+    return iFaceValue;
+  }
+
+  /**
+   * toString - Returns a string representation of this die
+   * 
+   * @param 
+   */
+  public String toString()
+  {
+    String sResult = Integer.toString(iFaceValue);
+
+    return sResult;
+  }
+}
