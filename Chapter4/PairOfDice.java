0681ee59c862937792ebad2555eadd1045ee42dc
LobsterCat <jacobro47@gmail.com>
2018-11-19 10:19:23 -0500

    Update PairOfDice.java

--- Chapter4/PairOfDice.java
+++ Chapter4/PairOfDice.java
@@ -2,7 +2,7 @@
 /**
  * PairOfDice.java
  *
- * Code Description: 
+ * Code Description:
  * 
  * @author Jacob Robbins
  * @version 11/19/18
@@ -10,14 +10,30 @@
  */
 
 public class PairOfDice
-  {
-  
+{
+
   private DieV2 dieOne, dieTwo;
-    
+
+  /**
+   * constructor - creates two object reference variables of the diev2 class
+   * 
+   * @param
+   */
   public PairOfDice()
   {
     dieOne = new DieV2();
     dieTwo = new DieV2();
   }
-  
+
+  /**
+   * roll method - rolls two dice and returns the sum
+   * 
+   * @param
+   */
+
+  public int roll()
+  {
+    return dieOne.roll() + dieTwo.roll();
+  }
+
 }
