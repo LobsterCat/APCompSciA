29bd1b43a3b3178454f9a22c72443ef985db7a16
LobsterCat <jacobro47@gmail.com>
2018-12-07 10:32:38 -0500

    Update Factorial.java

--- Chapter5/Factorial.java
+++ Chapter5/Factorial.java
@@ -26,15 +26,61 @@ public class Factorial
     iResult = 1;
     iCount = 1;
 
+    // initializing boolean to check if integer is negative
+    boolean bNegative, bAbove;
+
     // initializing Scanner to see how many times to print the statement
     Scanner kb = new Scanner(System.in);
 
     // prompting the user for a number to get its factorial
     System.out.println(
-        "Enter an integer between 0 and 31 to get the product of it as a factorial.");
+        "Enter an integer from 0 and 31 to get the product of it as a factorial.");
 
     // reading user input
     iFactorial = kb.nextInt();
+
+    // if statement sets negative to true if the nnumber is negative
+    // while loop loops the scanner prompt until the number entered is positive
+    if (Math.abs(iFactorial) != iFactorial)
+    {
+      bNegative = true;
+      while (bNegative == true)
+      {
+        System.out.println(
+            "This program does not deal with negative factorials. Please enter a positive integer.");
+        // loops within the while loop until the number entered is not negative
+        // prompting for a new number is in the while loop
+        iFactorial = kb.nextInt();
+        // rechecks if the number entered is positive or negative
+        if (Math.abs(iFactorial) == iFactorial)
+        {
+          // if above is true, number is no longer negative, program can proceed
+          bNegative = false;
+        }
+      }
+    }
+
+    // checks if the number entered is above 31
+    // loops and prompts the user until they enter a number lower than 32
+    // very similar to the previous loop
+    if (iFactorial > 31)
+    {
+      bAbove = true;
+
+      while (bAbove)
+      {
+        System.out.println();
+        System.out.println("This Java program cannot handle numbers above 31.");
+        System.out.println("Please enter a positive below 31.");
+        iFactorial = kb.nextInt();
+        // detects if the number is less than 32
+        if (iFactorial < 32)
+        {
+          // sets the while loop to false, breaking the cycle
+          bAbove = false;
+        }
+      }
+    }
 
     // loops until the counter is less than the factorial entered
     while (iCount < iFactorial)
@@ -43,6 +89,7 @@ public class Factorial
       iCount++;
     }
 
+    // prints the final answer of the factorial to the console
     System.out.println(iResult);
 
     kb.close(); // closing Scanner object
