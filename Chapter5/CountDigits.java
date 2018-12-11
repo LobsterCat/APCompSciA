94e0deb43dfecd0b7c89d551f555ce90360d845b
LobsterCat <jacobro47@gmail.com>
2018-12-11 10:09:22 -0500

    Create CountDigits.java

---
+++ Chapter5/CountDigits.java
@@ -0,0 +1,29 @@
+
+/**
+ * CountDigits.java
+ *
+ * Code Description: A program that reads an integer value from the user representing a year.
+ * 
+ * @author Jacob Robbins
+ * @version 12/11/18
+ * @contact 1002127@palisadessd.net
+ */
+
+import java.util.Scanner; // import Scanner class
+
+public class CountDigits
+{
+  /**
+   * main method - Uses remainders to determine what the year entered is
+   * divisible by, then prints to the console whether it is a leap year or not.
+   * 
+   * @param args
+   */
+  public static void main(String[] args)
+  {
+    // initializing Scanner for user input
+    Scanner kb = new Scanner(System.in);
+
+    kb.close(); // closing Scanner object
+  }
+}
